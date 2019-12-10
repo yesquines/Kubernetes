4c27c62469406b98bc4676372b53f1691b6fe403
Yago Ésquines <yago.almeida@4linux.com.br>
2019-12-10 19:21:20 -0300

    Reorganização do Repositorio

--- yamls/create_certificado.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-openssl genrsa -out kubeuser.key 2048 && \
-openssl req -new -key kubeuser.key -out kubeuser.csr -config csr.conf && \
-openssl x509 -req -in kubeuser.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out kubeuser.crt -days 365 -extensions v3_ext -extfile csr.conf
--- yamls/dashboard.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta6/aio/deploy/recommended.yaml
